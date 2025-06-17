41d49ac6441a85a85d24eba77f562d22107841b7
Faizatul Fitri <faizatulbtm01@gmail.com>
2025-06-18 01:25:39 +0800

    Set up Netlify function and updated blog list layout

---
+++ netlify/functions/blogProxy.js
@@ -0,0 +1,22 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+exports.handler = async function (event, context) {
+  try {
+    const res = await fetch("https://fi3blog.infinityfreeapp.com/wp-json/wp/v2/posts");
+    const data = await res.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: data
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Fetch failed", details: err.message })
+    };
+  }
+};
